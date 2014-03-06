ec6aeaaeabfeec7b95e0f1827c74d32e9a1d924e
Dominik Welland <hallo@doik.de>
2014-03-06 22:46:12 +0100

    optimize

--- problem1.jl
+++ problem1.jl
@@ -3,22 +3,10 @@
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-function find_3(max)
+function find_multiples(max)
     sum = 0
     for p=1:max-1
-        if p % 5 == 0
-            continue
-        elseif p % 3 == 0
-            sum += p
-        end
-    end
-    return sum
-end
-
-function find_5(max)
-    sum = 0
-    for p=1:max-1
-        if p % 5 == 0
+        if p % 5 == 0 || p % 3 == 0
             sum += p
         end
     end
@@ -27,4 +15,4 @@ end
 
 max = 1000
 
-println(find_3(max) + find_5(max))
+println(find_multiples(max))
